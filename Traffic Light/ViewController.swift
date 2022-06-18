d930abe8da84fa7db8db634ba7f85c654f80ddbc
Igor Sokolov <igor0401@icloud.com>
2022-06-19 00:29:28 +0300

    1. Making figures and its constraints. 2. Building apps logic.

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lightRed: UIView!
+    @IBOutlet var lightYellow: UIView!
+    @IBOutlet var lightGreen: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lightRed.layer.cornerRadius = 30.5
+        lightRed.alpha = 0.3
+        lightYellow.layer.cornerRadius = 30.5
+        lightYellow.alpha = 0.3
+        lightGreen.layer.cornerRadius = 30.5
+        lightGreen.alpha = 0.3
+        trafficLightButton.layer.cornerRadius = 10
     }
 
-
+   
+    @IBAction func trafficLightButtonTapped(_ sender: UIButton) {
+        
+        print("1")
+        
+        if lightRed.alpha == lightYellow.alpha &&
+            lightGreen.alpha == lightYellow.alpha {
+            
+            lightRed.alpha = 1
+            
+        } else if lightRed.alpha == 1 {
+            
+            lightRed.alpha = 0.3
+            lightYellow.alpha = 1
+            
+        } else if lightYellow.alpha == 1 {
+            
+            lightYellow.alpha = 0.3
+            lightGreen.alpha = 1
+            
+        } else if lightGreen.alpha == 1 {
+            
+            lightGreen.alpha = 0.3
+            lightRed.alpha = 1
+            }
+        
+        trafficLightButton.setTitle(
+            lightRed.alpha == 0.3 ? "Start" : "Next",
+            for: .normal
+        )
+    }
 }
 
